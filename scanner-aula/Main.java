1aebc600bac767729082ad4ef4e01680baebbdc6
Guilherme Canina <161065800+GuilhermeCanina@users.noreply.github.com>
2025-10-05 17:28:28 -0300

    Add files via upload

---
+++ scanner-aula/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String a, b;
+		
+		a = sc.nextLine();
+		
+		b = sc.nextLine();
+		
+		
+		System.out.println("Dados escritos: ");
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		sc.close();
+	}
+
+}
